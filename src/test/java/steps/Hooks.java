21093f313dc1043196e71970a436224a39d3b48e
Oleg <luckydev3000@gmail.com>
2017-09-18 14:52:54 -0500

    Jenkins fixing

--- src/test/java/steps/Hooks.java
+++ src/test/java/steps/Hooks.java
@@ -7,8 +7,10 @@ import cucumber.api.java.After;
 import cucumber.api.java.Before;
 import helpers.PropertiesFromFile;
 import org.apache.commons.io.FileUtils;
+import org.apache.log4j.Logger;
 import org.openqa.selenium.OutputType;
 import org.openqa.selenium.TakesScreenshot;
+import popups.CreateNewPostPopUp;
 
 import java.io.File;
 
@@ -17,12 +19,16 @@ import static com.codeborne.selenide.WebDriverRunner.getWebDriver;
 
 public class Hooks {
 
+    private final static Logger logger = Logger.getLogger(Hooks.class);
+
+
     @Before
     public void setupTimeout() {
         System.setProperty("webdriver.chrome.driver", PropertiesFromFile.getProperties().getProperty("driver.chrome.path"));
         Configuration.browser = PropertiesFromFile.getProperties().getProperty("browser");
         boolean headleass = WebDriverRunner.isHeadless();
         System.out.println(headleass);
+        logger.info("headless = " + headleass);
 
 
         //chromeOptions.addArguments("headless","disable-gpu")
